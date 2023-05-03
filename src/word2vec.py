585a1b7edf1c8162e2e93aad96cde9535da1fd60
amulet1989 <muletdelosreyes89@gmail.com>
2023-05-03 09:50:18 -0300

    Added Kmeans and vectorize with word2vec

--- src/word2vec.py
+++ src/word2vec.py
@@ -28,4 +28,18 @@ def vectorizer(
             document in the corpus.
     """
     # TODO
-    raise NotImplementedError
+    num_docs = len(corpus)
+    corpus_vectors = np.zeros((num_docs, num_features))
+
+    for i, doc in enumerate(corpus):
+        doc_vector = np.zeros((num_features,))
+        word_count = 0
+        for word in doc:
+            if word in model.wv:
+                doc_vector += model.wv[word]
+                word_count += 1
+        if word_count > 0:
+            doc_vector /= word_count
+        corpus_vectors[i] = doc_vector
+
+    return corpus_vectors
